4a65d969202377d1f2a3e8598b39a2a2a172a5e9
Lasse Boisen Andersen <la@laander.com>
2025-10-02 16:12:17 +0200

    deploy with nitro to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from './vite-plugins/netlify.ts'
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro-nightly/vite";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import netlify from "./vite-plugins/netlify.ts";
 
 const config = defineConfig({
-  plugins: [
-    netlify,
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
+	plugins: [
+		netlify,
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		nitro({ config: { preset: "vercel" } }),
+		viteReact(),
+	],
+});
 
-export default config
+export default config;
