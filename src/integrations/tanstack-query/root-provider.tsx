d1cea560ea0314209f99ce6ee5c5b9b88a39fe07
Lasse Boisen Andersen <la@laander.com>
2025-10-02 16:34:56 +0200

    one more attempt at SSR for vercel

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,20 +1,43 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+let queryClient: QueryClient;
+
+function makeQueryClient() {
+	return new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 60 * 1000,
+			},
+		},
+	});
+}
 
 export function getContext() {
-  const queryClient = new QueryClient()
-  return {
-    queryClient,
-  }
+	if (typeof window === "undefined") {
+		// Server: always make a new query client
+		return {
+			queryClient: makeQueryClient(),
+		};
+	}
+
+	// Browser: make a new query client if we don't already have one
+	if (!queryClient) {
+		queryClient = makeQueryClient();
+	}
+
+	return {
+		queryClient,
+	};
 }
 
 export function Provider({
-  children,
-  queryClient,
+	children,
+	queryClient,
 }: {
-  children: React.ReactNode
-  queryClient: QueryClient
+	children: React.ReactNode;
+	queryClient: QueryClient;
 }) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+	);
 }
